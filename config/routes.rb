e41aee874b09b1e68ebf631a7a619e79d32e7bda
rsundar <rohan.s.1987@gmail.com>
2019-12-30 17:59:09 +0000

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get 'static_pages/new_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
