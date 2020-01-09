0c7c1de603c4093abe5f38006372cb7b0d7efee1
rsundar <rohan.s.1987@gmail.com>
2020-01-10 03:54:23 +0530

    added signup route, and the signup page stub

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
   get '/new_page', to:'static_pages#new_page' 
+  get '/signup', to:'users#new'
 end
