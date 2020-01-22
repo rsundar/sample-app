0bb3fb79f07ad1188d35d3fba7533c8a4f826d08
rsundar <rohan.s.1987@gmail.com>
2020-01-23 00:10:22 +0530

    Validating user name input with TDD

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Someone", email:"someone@example.com")
+  end
+
+  test "should be valid user" do
+    assert @user.valid?
+  end
+
+  test "user's name should not be empty" do
+    @user.name = "       "
+    assert_not @user.valid?
+  end
 end
