fb3591604b8106f1864dfc196631003a01ef91f9
rsundar <rohan.s.1987@gmail.com>
2020-01-24 02:08:10 +0530

    Enforced case insensitive uniqueness at model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL = /\A[\w\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL }, uniqueness: true
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL }, uniqueness: { case_sensitive: false }
 end
