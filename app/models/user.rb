96508324934e9c87e6b476fc8a980f2a8f09b2da
rsundar <rohan.s.1987@gmail.com>
2020-01-24 22:34:38 +0530

    added uniquness at database indices level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL = /\A[\w\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL }, uniqueness: { case_sensitive: false }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,7 +34,6 @@ class UserTest < ActiveSupport::TestCase
 
   test "user emails are unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
