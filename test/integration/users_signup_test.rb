87f851189935348071c9b5a465edc55eddc934fd
rsundar <rohan.s.1987@gmail.com>
2020-02-14 04:30:03 +0530

    Add tests for valid signup in users_signup_test.rb

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,4 +17,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Riggatoni",
+                                         email: "riggi@riggatoni.com",
+                                         password: "foobar123",
+                                         password_confirmation: "foobar123" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
