53abb367b1c320bc02f76e91bc94343990c82f9d
rsundar <rohan.s.1987@gmail.com>
2020-02-23 23:10:02 +0530

    added logged in method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,8 +5,13 @@ module SessionsHelper
         session[:user_id]  = user.id
     end
 
-    # method to retrieve current user.
+    # method to retrieve current logged in user.
     def current_user(user)
         @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    # check if current user is logged in
+    def logged_in?
+        !current_user.nil?
     end
 end
