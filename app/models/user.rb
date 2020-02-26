3a38f2233247666ead81f5bbad5e32454e0170c4
rsundar <rohan.s.1987@gmail.com>
2020-02-26 23:16:26 +0530

    error correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
     validates :password, presence: true, length: { minimum: 8 }
 
     def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine:MIN_COST : BCrypt::Engine.cost
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
 end
