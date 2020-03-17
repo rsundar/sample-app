23d9b09b312d07582de2d701521d43681b6119fe
rsundar <rohan.s.1987@gmail.com>
2020-03-18 02:41:15 +0530

    error correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect the user to the user's show page
-      log_in(user)
+      log_in user
       redirect_to user
     else
       # Create an error message
